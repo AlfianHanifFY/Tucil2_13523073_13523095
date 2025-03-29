8d0338e8dbf02c58d00e2545e708d3b873ccf480
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-03-29 16:44:30 +0700

    feat: add splitImage

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ by alfian & rafif
 # how to run
 
 javac -d bin -cp src $(find src -name "\*.java")
+
 <br>
 java -cp bin Main
--- src/lib/Image.java
+++ src/lib/Image.java
@@ -26,6 +26,25 @@ public class Image {
         this.matrix = new Pixel[_rows][_cols];
     }
 
+    // cctor
+    public Image(Image img, int _rows, int _cols, int rowStart, int colStart) {
+        this.rows = _rows;
+        this.cols = _cols;
+        this.variance = img.variance;
+        this.treshold = img.treshold;
+        this.minimumBlockSize = img.minimumBlockSize;
+        this.matrix = new Pixel[_rows][_cols];
+
+        for (int i = 0; i < getRow(); i++) {
+            for (int j = 0; j < getCol(); j++) {
+                Pixel originalPixel = img.getPixel(rowStart + i, colStart + j);
+                Pixel p = new Pixel(originalPixel);
+                setPixel(i, j, p);
+            }
+        }
+
+    }
+
     public void setImageParam(int _variance, double _treshold, int _minimumBlockSize) {
         this.variance = _variance;
         this.treshold = _treshold;
@@ -48,13 +67,42 @@ public class Image {
         return this.cols;
     }
 
-    public void print() {
-        for (int i = 0; i < getRow(); i++) {
+    public void print(int limitRow) {
+        int R;
+        if (limitRow == 0) {
+            R = getRow();
+        } else {
+            R = limitRow;
+        }
+        for (int i = 0; i < R; i++) {
             for (int j = 0; j < getCol(); j++) {
                 Pixel p = getPixel(i, j);
                 p.print();
                 System.out.println();
             }
         }
+    }
+
+    public Image[] splitImage() {
+        // membagi image menjadi 4 bagian
+
+        /*
+         * ILUSTRASI SPLIT
+         * 
+         * collection[0] | collection[1]
+         * -------------------------------
+         * collection[2] | collection[3]
+         * 
+         */
+        Image[] collection = new Image[4];
+        int midRow = rows / 2;
+        int midCol = cols / 2;
+
+        collection[0] = new Image(this, midRow, midCol, 0, 0); // Top-left
+        collection[1] = new Image(this, midRow, cols - midCol, 0, midCol); // Top-right
+        collection[2] = new Image(this, rows - midRow, midCol, midRow, 0); // Bottom-left
+        collection[3] = new Image(this, rows - midRow, cols - midCol, midRow, midCol); // Bottom-right
+
+        return collection;
     }
 }
