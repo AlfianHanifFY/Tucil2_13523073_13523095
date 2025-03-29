9ab52e6766e1a75fdc074d3cb00ee802b6780e6c
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-03-29 12:09:53 +0700

    feat: add main

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+import lib.*;
+
+public class main {
+    static Scanner inputScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("\u001B[34m[INFO]\u001B[0m" + " : Wilujeng Sumping...\n");
+    }
+}
