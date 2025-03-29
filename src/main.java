b8f0c15aed00e212c53ebf194b1e81a0abaa9133
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-03-29 13:37:08 +0700

    feat: add input image

--- src/main.java
+++ src/main.java
@@ -6,6 +6,11 @@ public class Main {
     static Scanner inputScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("\u001B[34m[INFO]\u001B[0m" + " : Wilujeng Sumping...\n");
+        IO io;
+        Image image;
+        System.out.println("\u001B[34m[INFO]\u001B[0m" + " : Wilujeng Sumping...\n\n");
+        System.out.println("\u001B[34m[INFO]\u001B[0m" + " : Masukkan alamat image ! \n");
+        image = IO.readImage();
+        image.print();
     }
 }
