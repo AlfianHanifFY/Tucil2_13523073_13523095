8ed4341712b5bb96785833ffb1e25d178e1f975b
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-03-29 12:14:49 +0700

    chore: refactor main

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 import lib.*;
 
-public class main {
+public class Main {
     static Scanner inputScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
