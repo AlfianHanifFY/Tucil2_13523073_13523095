32a0621331131103cce42d33a93b4a2134ff8648
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-04-10 04:21:54 +0700

    chore: refactor

--- src/lib/Compressor.java
+++ src/lib/Compressor.java
@@ -4,21 +4,21 @@ public class Compressor {
 
     public static void compress(Quadtree qt) {
         qt.calcAvgPixel();
-        if (qt.getCol() * qt.getRow() <= IO.infoImage.minimumBlockSize
-                || qt.getCol() / 2 * qt.getRow() / 2 <= IO.infoImage.minimumBlockSize) {
-            return;
-        }
-
         double variance = IO.infoImage.getErrorMeasurement(qt.getStartRow(), qt.getStartCol(), qt.getRow(),
                 qt.getCol(), qt.getAvgPixel());
 
-        // System.out.println("Variance: " + variance);
-        if (variance > IO.infoImage.treshold) {
+        if (qt.getCol() * qt.getRow() <= IO.infoImage.minimumBlockSize
+                || qt.getCol() / 2 * qt.getRow() / 2 <= IO.infoImage.minimumBlockSize) {
+            return;
+        } else if (variance <= IO.infoImage.treshold) {
+            return;
+        } else {
             qt.split();
             compress(qt.getQ1());
             compress(qt.getQ2());
             compress(qt.getQ3());
             compress(qt.getQ4());
         }
+
     }
 }
