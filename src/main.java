2774db48f6e4f2cc01c23b09bd6d78b6c7a3b8ee
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-04-09 07:58:50 +0700

    chore: rename func

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import java.awt.image.BufferedImage;
 import java.util.*;
 import lib.*;
 
-public class Main {
+public class main {
     static Scanner inputScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
